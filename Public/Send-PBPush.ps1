d7432440abcbcbf06e3f0cf1eeea73e4f17dfffd
Scott Evtuch <scott@evtuch.net>
2017-01-16 14:56:09 -0600

    Implement Send-PBPush

---
+++ Public/Send-PBPush.ps1
@@ -0,0 +1,63 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+#>
+function Send-PBPush
+{
+    [CmdletBinding(DefaultParameterSetName='UserDevices', 
+                  SupportsShouldProcess=$true,
+                  ConfirmImpact='Medium')]
+    [Alias()]
+    Param
+    (
+        # Device to send the push to
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Push,
+
+        # Device to send the push to
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ParameterSetName='UserDevice')]
+        [ValidateNotNullOrEmpty()]
+        [Alias("device_iden")]
+        $DeviceID,
+
+        # Email to send the push to
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ParameterSetName='Email')]
+        [ValidateNotNullOrEmpty()]
+        $Email,
+
+        # Email to send the push to
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ParameterSetName='ChannelTag')]
+        [ValidateNotNullOrEmpty()]
+        [Alias("channel_tag")]
+        $ChannelTag,
+
+        # Email to send the push to
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ParameterSetName='Client')]
+        [ValidateNotNullOrEmpty()]
+        [Alias("client_id")]
+        $ClientID
+    )
+
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Push", "Send"))
+        {
+            # Invoke the API
+            Invoke-PBAPI -RelativePath '/pushes' -Method Post -Body $Push
+        }
+    }
+}
