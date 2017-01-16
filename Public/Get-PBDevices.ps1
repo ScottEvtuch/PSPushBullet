b94429b4cc2fe8cc0552b09dde72bea9f823e66d
Scott Evtuch <scott@evtuch.net>
2017-01-16 15:28:37 -0600

    Implement Get-PBDevices

---
+++ Public/Get-PBDevices.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+#>
+function Get-PBDevices
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param()
+
+    Process
+    {
+        # Invoke the API
+        $Response = Invoke-PBAPI -RelativePath '/devices' -Method Get
+
+        # Return the response
+        return $Response.devices
+    }
+}
