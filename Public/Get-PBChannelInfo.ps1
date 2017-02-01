b33b1eb28f2722849cbbf1cd225c6bb4a04b1299
Scott Evtuch <scott@evtuch.net>
2017-02-01 14:21:32 -0600

    Implement Get-PBChannelInfo

---
+++ Public/Get-PBChannelInfo.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+#>
+function Get-PBChannelInfo
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # The channel to get information for
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ParameterSetName='ChannelTag')]
+        [ValidateNotNullOrEmpty()]
+        [Alias("channel_tag")]
+        [String]
+        $ChannelTag
+    )
+
+    Process
+    {
+        Write-Verbose "Building the request body"
+        $Body = @{
+            "tag" = "$ChannelTag";
+        }
+
+        Write-Verbose "Invoking Invoke-PBAPI on the channel"
+        Invoke-PBAPI -RelativePath '/channel-info' -Method Get -Body $Body
+    }
+}
